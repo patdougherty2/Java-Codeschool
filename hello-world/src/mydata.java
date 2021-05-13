81aae51125ed24b90630a315baff653dfa05d700
patdougherty2 <patdougherty2@gmail.com>
2021-05-13 17:11:16 -0500

    test

--- hello-world/src/mydata.java
+++ hello-world/src/mydata.java
@@ -1,4 +1,4 @@
-public class helloworld {
+public class mydata {
 
     public static void main(String[] args) {
 
---
+++ hello-world/src/names.java
@@ -0,0 +1,9 @@
+public class names {
+
+    public static void main(String[] args) {
+
+        System.out.println("My name2");
+
+    }
+
+}
